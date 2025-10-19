b3dcb2021dcc4c8576b369731c027b0d055473cb
Adam <abobich675@gmail.com>
2025-10-19 01:39:01 -0700

    start on alphabet blocks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import { generateImage } from "./actions";
 import { useState } from "react";
 import { toast } from "sonner";
 import FormattedAscii from "@/components/ui/FormattedAscii";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
 
 
 type AsciiAndType = {
@@ -17,6 +19,7 @@ export default function Home() {
   
   const [asciiList, setASCIIList] = useState<AsciiAndType[]>([])
   const [style, setStyle] = useState<string>("bw")
+  const [loading, setLoading] = useState(false)
 
   let prompt = ""
 
@@ -25,6 +28,7 @@ export default function Home() {
       return
     
     const fetchGeneration = async () => {
+      setLoading(true)
       const currStyle = style
       const res = await generateImage(prompt)
       if (!res) {
@@ -32,6 +36,7 @@ export default function Home() {
       } else {
         setASCIIList([...asciiList, {ascii:res, style: currStyle}])
       }
+      setLoading(false)
     }
     fetchGeneration()
   }
@@ -44,18 +49,30 @@ export default function Home() {
       </div>
       <div className="flex justify-center pt-10 pb-10">
         <div className="w-[90%]">
-          <div className="flex justify-center pb-10">
-            <div className="flex w-full max-w-sm items-center gap-2">
-              <Input placeholder="ASCII Image Prompt..." onChange={(e) => prompt = e.target.value}/>
-              <Button type="button" variant="outline" onClick={() => generate()}>
-                Generate
-              </Button>
-              <select name="style" value={style} onChange={(e) => setStyle(e.target.value)}
-                className="border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 h-9 px-4 py-2 has-[>svg]:px-3 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive">
-                <option value="color">Colored</option>
-                <option value="bw">Black & White</option>
-              </select>
+          <div className="flex w-full justify-center gap-5">
+            <div>
+              hello
             </div>
+            <div>
+              <div className="flex w-max items-center gap-2">
+                <Input className="w-100" placeholder="ASCII Image Prompt..." onChange={(e) => prompt = e.target.value}/>
+                <select name="style" value={style} onChange={(e) => setStyle(e.target.value)}
+                  className="border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 h-9 px-4 py-2 has-[>svg]:px-3 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive">
+                  <option value="color">Colored</option>
+                  <option value="bw">Black & White</option>
+                </select>
+              </div>
+              <div className="flex justify-center pb-10 pt-2">
+                <Button className="w-138" type="button" variant="outline" disabled={loading} onClick={() => generate()}>
+                  {loading ? (
+                    <Loader2 size={20} className="animate-spin mx-auto" />
+                  ) : (
+                    "Generate"
+                  )}
+                </Button>
+              </div>
+            </div>
+            <Image src="/alphabet_blocks.svg" alt="Alphabet blocks" width={200} height={200}/>
           </div>
           <div className="flex flex-wrap gap-5 justify-center">
             {[...asciiList].reverse().map((ascii, index) => (
