ac96b7db261e809091cad145fb1e5cba2252e190
Adam <abobich675@gmail.com>
2025-10-19 02:01:49 -0700

    fix input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,8 +20,7 @@ export default function Home() {
   const [asciiList, setASCIIList] = useState<AsciiAndType[]>([])
   const [style, setStyle] = useState<string>("bw")
   const [loading, setLoading] = useState(false)
-
-  let prompt = ""
+  const [prompt, setPrompt] = useState("")
 
   function generate() {
     if (prompt == "")
@@ -55,7 +54,7 @@ export default function Home() {
             </div>
             <div>
               <div className="flex w-max items-center gap-2">
-                <Input className="w-100" placeholder="ASCII Image Prompt..." onChange={(e) => prompt = e.target.value}/>
+                <Input className="w-100" placeholder="ASCII Image Prompt..." value={prompt} onChange={(e) => setPrompt(e.target.value || "")}/>
                 <select name="style" value={style} onChange={(e) => setStyle(e.target.value)}
                   className="border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 h-9 px-4 py-2 has-[>svg]:px-3 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive">
                   <option value="bw">Black & White</option>
